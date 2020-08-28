0fa3dff330bece5636d0f1cf90ca042321265b1c
Ridwan <ridwanishola97@gmail.com>
2020-08-28 17:12:42 +0100

    test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    hi
+    hi world
     <App />
   </React.StrictMode>,
   document.getElementById('root')
