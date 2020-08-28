1066a438a13dd133212d02a867ef61f1496b6bd4
Ridwan <ridwanishola97@gmail.com>
2020-08-28 17:37:06 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api = {
+    key: '9e6f6612e4185979dbaeb5b588ed1a81',
+    base: 'https://api/openweathermap.org/data/2.5'
+
 }
+class App extends Component {
+
+    render() {
+        return (
+            <div className='App'>
+                Hello wow
+            </div>
+        )
+    }
+}
+
 
 export default App;
